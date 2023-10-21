9c745edab3e76169cb357d7c2278b2197db6ea95
jano <jmezo.asd@gmail.com>
2023-10-21 16:58:36 +0200

    configure gitsigns

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -2,7 +2,12 @@ return {
 	{ 'tpope/vim-commentary' },
 	{ 'tpope/vim-surround' },
 	{ 'tpope/vim-fugitive' },
-	{ 'lewis6991/gitsigns.nvim' },
+	{
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end,
+  },
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -23,7 +28,7 @@ return {
   {
 		'nvim-telescope/telescope.nvim',
 		dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function(_, opts)
+    config = function()
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>p', builtin.find_files, {})
       vim.keymap.set('n', '<leader>rg', builtin.live_grep, {})
