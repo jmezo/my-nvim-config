de407b7b6b6a3af344daf57a2e8d784b6ac0cdc6
jano <jmezo.asd@gmail.com>
2023-10-21 20:51:26 +0200

    add trouble and diagnostic options

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -34,11 +34,14 @@ return {
 
       -- Use LspAttach autocommand to only map the following keys
       -- after the language server attaches to the current buffer
+      -- and configure diagnostics display
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('UserLspConfig', {}),
         callback = function(ev)
           -- Enable completion triggered by <c-x><c-o>
           vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+          vim.lsp.diagnostic.opts = { virtual_text = false }
 
           -- Buffer local mappings.
           -- See `:help vim.lsp.*` for documentation on any of the below functions
@@ -58,6 +61,7 @@ return {
           vim.keymap.set('n', '<space>wl', function()
             print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
           end, opts)
+          vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
         end,
       })
     end,
