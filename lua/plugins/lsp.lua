151f7d37b10fbe2c1760b02485e37afe3a0b8407
jano <jmezo.asd@gmail.com>
2023-10-22 09:27:23 +0200

    disable checkThirdParty in lua workspace

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,6 +23,8 @@ return {
             workspace = {
               -- Make the server aware of Neovim runtime files
               library = vim.api.nvim_get_runtime_file("", true),
+              -- https://github.com/neovim/nvim-lspconfig/issues/1700#issuecomment-1033127328
+              checkThirdParty = false,
             },
             -- Do not send telemetry data containing a randomized but unique identifier
             telemetry = {
