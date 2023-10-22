dea596d5cdafb01d5141fe2c6888c3ebf0bec74c
jano <jmezo.asd@gmail.com>
2023-10-22 15:01:18 +0200

    show hidden files in neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,6 +6,13 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      },
+    },
+  },
   keys = {
     {
       "<leader>b",
