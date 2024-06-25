6f8260aa8a39aadfd948e15f09014e8f3b45db9b
jano <jmezo.asd@gmail.com>
2024-06-25 22:37:22 +0200

    replace gitsigns with gitgutter

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -2,10 +2,11 @@ return {
   { 'tpope/vim-commentary' },
   { 'tpope/vim-surround' },
   { 'tpope/vim-fugitive' },
-  {
-    'lewis6991/gitsigns.nvim',
-    config = function()
-      require('gitsigns').setup()
-    end,
-  },
+  { 'airblade/vim-gitgutter' },
+  -- {
+  --   'lewis6991/gitsigns.nvim',
+  --   config = function()
+  --     require('gitsigns').setup()
+  --   end,
+  -- },
 }
