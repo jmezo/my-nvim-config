98f2884ead59028e8f5f2ee5b59bcc61a4aee8ba
jano <jmezo.asd@gmail.com>
2025-01-12 10:18:40 +0100

    update plugins, add hardtime

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,8 @@
+-- :Hardtime enable enable hardtime.nvim
+-- :Hardtime disable disable hardtime.nvim
+-- :Hardtime toggle toggle hardtime.nvim
+return {
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {}
+}
